150f12881068cc3a9fdcf192c166fd889b7fe656
JayKim88 <mearak43@gmail.com>
2021-05-29 08:30:40 +0900

    [Client]ADD Mainnav Section
    - Layout / CSS without logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import MainNav from "./sections/MainNav";
+import styled, { ThemeProvider } from "styled-components";
+import defaultTheme from "./common/style/themes/default";
 
 function App() {
   return (
-    <div className="App">
-      J Portfolio
-    </div>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Main>
+          <MainNav />
+        </Main>
+      </ThemeProvider>
+    </>
   );
 }
+
+const Main = styled.div`
+  border: 3px solid green;
+  height: 100%;
+`;
 
 export default App;

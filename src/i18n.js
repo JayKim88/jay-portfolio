fb1b9d1b25687de33083b83be712b6599144a2bb
Yongjae Kim <mearak43@gmail.com>
2025-06-02 22:56:11 +0900

    fix: how to use translation resource

--- src/i18n.js
+++ src/i18n.js
@@ -1,25 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
+import ko from "./locales/ko/translation.json";
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: "en",
-    fallbackLng: "en",
-    debug: false,
-    saveMissing: false,
-    missingKeyHandler: null,
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-    returnNull: false,
-    returnEmptyString: false,
-    returnObjects: false,
-  });
+i18n.use(initReactI18next).init({
+  lng: "en",
+  debug: false,
+  resources: {
+    ko: { translation: ko },
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+  returnNull: false,
+  returnEmptyString: false,
+  returnObjects: false,
+});
 
 export default i18n;
